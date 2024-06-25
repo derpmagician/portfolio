2dd1381ef26843775dbef5d2abd49e72126ebd93
derpmagician <renzin@gmail.com>
2024-06-25 09:23:17 -0500

    actualizando cv

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 import './i18n';
-
+// https://derpmagician.github.io/portfolio
+// https://renzoperezt.net/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
